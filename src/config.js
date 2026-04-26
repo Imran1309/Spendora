30043964d9f8f166605b7b2585481d51ecf78751
imran.s.s <imranss1309@gmail.com>
2026-04-26 21:36:03 +0530

    Update API_URL to public localtunnel for APK access

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://10.25.198.38:5000';
+export const API_URL = process.env.EXPO_PUBLIC_API_URL || 'https://chilly-shoes-decide.loca.lt';
